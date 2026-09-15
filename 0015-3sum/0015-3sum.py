2ba435786fb10c89d93af3baa9a20eb96d1a2ab5
Vanshika Malik <vanshikamalik0918@gmail.com>
2026-09-16 02:15:02 +0530

    Time: 1369 ms (11.91%), Space: 23.8 MB (5.61%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,42 @@
+# This is the classic example of two pointers here what happen is i pointer remin fix and j and k pointers move . so treat j as left pointer and k as right pointer now do similar to two sum like sum==0 so nums[i]+nums[j]+nums[k]==0 if they are equal to zero then append it into resukt . if sum is greater than zero than k-=1 if sum is less then 0 then j+=1 then at last return the result
+
+###     Briteforce approach it will solve in O(n^3)but woth optimixw it willl solve in O(n^2)
+# class Solution:
+    # def threeSum(self, nums: List[int]) -> List[List[int]]:
+    #     result = []
+
+    #     for i in range(len(nums) - 1):
+    #         for j in range(i + 1, len(nums)):
+    #             for k in range(j + 1, len(nums)):
+    #                 if nums[i] + nums[j] + nums[k] == 0:
+    #                     result.append([nums[i], nums[j], nums[k]])
+
+    #     return result
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = set()
+#Here after sorting we used set function so the result does not caontain any deuplicte value but keep in mind at last conver this set into list 
+# using 
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total == 0:
+                    # since i am using set for removing suplciate then instead of append i must use add and since i am using add so i must convert my list into tuple 
+                    # before it is result.append([nums[i],nums[j],nums[k]]) but now list in tuple
+                    result.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+
+                elif total < 0:
+                    j += 1
+
+                else:
+                    k -= 1
+
+        return [list(x) for x in result] # it is just a way to do with tuple into list 
