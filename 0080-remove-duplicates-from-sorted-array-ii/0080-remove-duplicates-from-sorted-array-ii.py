a1080ccbf49ab26cf0629aa895d4a7e0e560cf1d
Vanshika Malik <vanshikamalik0918@gmail.com>
2026-09-19 03:25:38 +0530

    Time: 7 ms (97.56%), Space: 21.2 MB (97.26%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,26 @@
+#if left < 2:
+#Always allow the first two numbers.
+#nums[right] != nums[left - 2]
+# means:
+# "Is this number different from the number two positions behind in my result?"
+# If yes → keep it.
+# If no → it's the third or later occurrence, so skip it.
+# Example
+# Input:
+# [0,0,1,1,1,1,2,3,3]
+# Output portion:
+# [0,0,1,1,2,3,3]
+# left is the pointer that builds the valid answer.
+# right is the pointer that scans every element.
+
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        left = 0
+
+        for right in range(len(nums)):
+            if left < 2 or nums[right] != nums[left - 2]:
+                nums[left] = nums[right]
+                left += 1
+
+        return left
