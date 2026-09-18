6e1c25b7cdc91fad99b90e5662ca6bb01e7a1b4c
Vanshika Malik <vanshikamalik0918@gmail.com>
2026-09-19 01:33:41 +0530

    Time: 3 ms (44.53%), Space: 20.5 MB (43.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,3 +1,9 @@
+# okay so this question is easy here simple whats happening we dont care if 1==1 on similar element we are not doing anything but 
+# if i=j so we got anew elemnt here right so 
+# j will say to i bhai i got a new element here 
+# then i will say ok bhai i will make space for that new elemnt so i++
+# now i had make a spce so we will put that j value in i means nums[i]=nums[j]
+# now j will again move to find a new elemtn and similarly if i and j are equal nothung j move forward but agin if j got new elemtn then i will make space and gonna interchage their values .
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
         left = 0
