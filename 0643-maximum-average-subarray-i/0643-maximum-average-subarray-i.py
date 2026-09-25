2dbab32099faefffea58aed055130448f97d45bb
Vanshika Malik <vanshikamalik0918@gmail.com>
2026-09-25 06:17:21 +0530

    Time: 83 ms (23.14%), Space: 29.2 MB (36.04%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -10,7 +10,9 @@ class Solution:
         window_avg=0
         left=0
         right=0
-        max_avg=float('-inf')# here max_avg = 0 it can also be used but with this it only pas 121/128 test case with this float we can also put negative numbers here
+        max_avg=float('-inf')# here max_avg = 0 it can also be used but with this it only pas 121/128 test case with this float we can also put negative numbers here 
+#Maximum of possibly negative numbers → float('-inf') 
+#Minimum of possibly positive numbers → float('inf')
         for right in range (len(nums)):
             window_avg+=nums[right]
             if right-left+1==k:
